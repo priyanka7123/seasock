51a843c9b745b49f215932e36c4f9227cedad449
priyanka <priyankadas.7123@gmail.com>
2020-04-30 13:55:45 +0530

    commit

---
+++ include/nav.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+        <a href="" class="navbar-brand">seasock</a>
+        <form action="" class="form-inline mx-auto">
+            <input type="search" class="form-control" size="70">
+            <input type="submit" class="btn btn-dark">
+            
+        </form>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item"><a href="" class="nav-link">home</a></li>
+            <li class="nav-item"><a href="" class="nav-link">profile</a></li>
+            <li class="nav-item"><a href="" class="nav-link">logout</a></li>
+        </ul>
+    </div>
+</nav>
--- profile.php
+++ profile.php
@@ -6,8 +6,31 @@
     <link href="css/bootstrap.css" rel="stylesheet">
 </head>
 <body class="bg-light">
-<nav class="navbar navbar-expand-lg navbar-dark">
-    
-</nav>
+<?php include"include/nav.php";?>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-lg-3">
+            <div class="card">
+               <img src="https://via.placeholder.com/250" alt="">
+                <div class="card-body">
+                  <h2 class="h5 text-uppercase text-center">priyanka</h2>  
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="card">
+                
+                    <form action="">
+                        <textarea name="" id="" cols="30" rows="6" class="form-control" placeholder="write something here"></textarea>
+                    </form>
+                <div class="card-body">
+                    <input type="file">
+                    <input type="submit" class="btn btn-primary btn-sm float-right">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
