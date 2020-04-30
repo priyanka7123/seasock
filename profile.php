13e5741ceb237f4536bec20cc313bb0e87e7beb8
priyanka <priyankadas.7123@gmail.com>
2020-04-30 13:02:54 +0530

    second commit

---
+++ profile.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>seasock |  connect Together</title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+    </body>
+</html>
